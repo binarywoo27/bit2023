f3ed366c6951f0176b5f76f28384b24b0935a3d6
blu_m <blu_m@LAPTOP-C60S8SS4>
2023-08-01 15:15:58 +0900

    20230801-2

--- day01/src/day01/Main.java
+++ day01/src/day01/Main.java
@@ -3,7 +3,8 @@ package day01;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello2222"); 
+		System.out.println("Hello1111"); 
+		System.out.println("Hello2222");
 	}
 
 }
